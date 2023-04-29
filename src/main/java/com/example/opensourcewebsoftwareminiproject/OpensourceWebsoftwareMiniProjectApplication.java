cd351be8f44f4edb048bd3fa38a6230c82501bdb
0Chord <seaweed.0chord@gmail.com>
2023-04-29 23:12:26 +0900

    feat : 추가 opensource-webminiproject

---
+++ src/main/java/com/example/opensourcewebsoftwareminiproject/OpensourceWebsoftwareMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.opensourcewebsoftwareminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensourceWebsoftwareMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensourceWebsoftwareMiniProjectApplication.class, args);
+	}
+
+}
